450ecb918d57e51cb8d017286eb58fc26ac611db
Stephen Pearce <hello@stevepdp.dev>
2023-01-15 13:29:41 +0000

    feat(minigame): add main game UI elements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Player player;
+    [SerializeField] TMP_Text playerScoreText;
+
+
+}
