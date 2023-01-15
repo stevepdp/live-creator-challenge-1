e835ccf3db75007eca10f21047d12d9613651265
Stephen Pearce <hello@stevepdp.dev>
2023-01-15 13:52:03 +0000

    feat(minigame): add click and 60sec countdown functionality

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Clicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Player player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void IncrementScore() => player.IncrementScore();
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,27 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] Player player;
     [SerializeField] TMP_Text playerScoreText;
+    [SerializeField] private float timeRemaining = 60f;
 
+    private void Start()
+    {
+        StartCoroutine(CountdownTimer());
+    }
+    private void Update()
+    {
+        UpdateScore();
+    }
 
+    void UpdateScore() => playerScoreText.text = player.PlayerScore.ToString();
+
+    IEnumerator CountdownTimer()
+    {
+        while (timeRemaining > 0)
+        {
+            timeRemaining -= 1;
+            Debug.Log("Time remaining: " + timeRemaining);
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("-> to leaderboard");
+    }
 }
